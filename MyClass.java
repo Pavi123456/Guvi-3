6dd99954cb58bcfebd9be02417bac460ff01970f
Anubhav kumar <anubhavcse08@users.noreply.github.com>
2018-02-13 18:44:38 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MyClass {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str=sc.next();
+
+        if(str.equals("Sunday") || str.equals("Sunday")){
+
+            System.out.println("YES");
+
+        }
+
+        else if(str.equals("Monday") || str.equals("Tuesday") || str.equals("Wednesday") || str.equals("Thursday") || str.equals("Friday")){
+
+            System.out.println("NO");
+
+        }
+
+        else{
+
+            System.out.println("It's not day....");
+
+        }
+
+    }
+
+}
