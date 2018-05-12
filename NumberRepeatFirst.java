55ff2447e49ab5f430d27a5cf7fb04682fc27e1a
Anubhav kumar <anubhavcse08@users.noreply.github.com>
2018-05-13 02:18:31 +0530

    Add files via upload

--- NumberRepeatFirst.java
+++ NumberRepeatFirst.java
@@ -4,19 +4,22 @@ public class NumberRepeatFirst {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int result = 0;
 		int n = sc.nextInt();
 		int[] arr = new int[n];
 		for(int i=0;i<n;i++) {
 			arr[i] = sc.nextInt();
 		}
+		outer:
 		for(int i=0;i<arr.length;i++) {
 			for(int j=i+1;j<arr.length;j++) {
 				if(arr[i]==arr[j]) {
-					System.out.println((arr[i]));
-					break;
+						break outer;
 				}
+				result = arr[i];
 			}
 		}
+		System.out.println(result);
 	}
 
 }
